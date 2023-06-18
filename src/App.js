8c624fb563191f6ef9415c30af6ca8bfb7bdfaab
armeen <armeenfarange@yahoo.com>
2023-06-18 14:50:19 -0700

    Add fields, Update CSS, and Contact page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Home from './pages/Home/home';
 import About from './pages/About/about';
+import Contact from './pages/Contact/contact';
 // import { FaGithubSquare, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 function App() {
@@ -17,12 +18,16 @@ function App() {
             <NavLink className="navlink" exact activeClassName="active" to="about">
               About
             </NavLink>
+            <NavLink className="navlink" exact activeClassName="active" to="contact">
+            Contact
+            </NavLink>
           </div>
         </nav>
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/About" element={<About />} />
+            <Route path="/Contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,20 +3,27 @@ import './home.css';
 import { FaChevronDown } from 'react-icons/fa';
 import kirklandImage from './images/Kirkland.png';
 import redmondImage from './images/Redmond.png';
+import { kirklandSoccerFields, redmondSoccerFields } from './soccerFields.js';
 
 const Home = () => {
     const [showCityInfo, setShowCityInfo] = useState({ kirkland: false, redmond: false });
-    const [showMap, setShowMap] = useState(false); // New state for iframe visibility
+    const [kirklandFields, setKirklandFields] = useState(kirklandSoccerFields);
+    const [redmondFields, setRedmondFields] = useState(redmondSoccerFields);
 
     const handleCityClick = (city) => {
         setShowCityInfo({ ...showCityInfo, [city]: !showCityInfo[city] });
-
-        // Reset the iframe visibility on city click
-        setShowMap(false);
     };
 
-    const handleShowMapClick = () => {
-        setShowMap(!showMap);
+    const handleShowMapClick = (city, index) => {
+        if (city === 'kirkland') {
+            const updatedFields = [...kirklandFields];
+            updatedFields[index].showMap = !updatedFields[index].showMap;
+            setKirklandFields(updatedFields);
+        } else if (city === 'redmond') {
+            const updatedFields = [...redmondFields];
+            updatedFields[index].showMap = !updatedFields[index].showMap;
+            setRedmondFields(updatedFields);
+        }
     };
 
     return (
@@ -34,24 +41,27 @@ const Home = () => {
                         <div>
                             <h1 className='city-info-info'>Fields</h1>
                             <p className='city-info-info header-other-names-bold'>----------</p>
-                            <p className='city-info-info header'>Lakeview - Click <a href="https://secure.rec1.com/WA/city-of-kirkland/Lakeview-Elementary/6577cal">Here</a> For Schedule</p>
-                            <p className='city-info-info header-other-names'>Other names: Lakeview Elementary, Google</p>
-                            <p className='city-info-info header-other-names'><a href="https://goo.gl/maps/BG9fCFnWbfZr5TE26">Address</a></p>
-                            <button className="show-map-button button" onClick={handleShowMapClick}>
-                                {showMap ? 'Hide Map' : 'Show Map'}
-                            </button>
-                            {showMap && (
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1343.4135928757325!2d-122.20061533742911!3d47.668353494764354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549013e01728085f%3A0x6c5821dad5367e3b!2sLakeview%20Elementary%20Sports%20Fields!5e0!3m2!1sen!2sus!4v1687050099318!5m2!1sen!2sus"
-                                    width="600"
-                                    height="450"
-                                    title="Lakeview Elementary Sports Fields"
-                                    allowFullScreen=""
-                                    loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                ></iframe>
-                            )}
                         </div>
+                        {kirklandFields.map((field, index) => (
+                            <div key={index}>
+                                <p className='city-info-info header'>{field.name} - Click <a href={field.scheduleLink}>Here</a> For Schedule</p>
+                                <p className='city-info-info header-other-names'><a href={field.addressLink}>Address</a></p>
+                                <button className="show-map-button button" onClick={() => handleShowMapClick('kirkland', index)}>
+                                    {field.showMap ? 'Hide Map' : 'Show Map'}
+                                </button>
+                                {field.showMap && (
+                                    <iframe
+                                        src={field.mapEmbedUrl}
+                                        width="600"
+                                        height="450"
+                                        title={field.name}
+                                        allowFullScreen=""
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    ></iframe>
+                                )}
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
@@ -63,13 +73,34 @@ const Home = () => {
                     <div className="city-info">
                         <img src={redmondImage} alt="Redmond" className="city-image" />
                         <div>
-                            <p>City information for Redmond goes here...</p>
+                            <h1 className='city-info-info'>Fields</h1>
+                            <p className='city-info-info header-other-names-bold'>----------</p>
                         </div>
+                        {redmondFields.map((field, index) => (
+                            <div key={index}>
+                                <p className='city-info-info header'>{field.name} - Click <a href={field.scheduleLink}>Here</a> For Schedule</p>
+                                <p className='city-info-info header-other-names'><a href={field.addressLink}>Address</a></p>
+                                <button className="show-map-button button" onClick={() => handleShowMapClick('redmond', index)}>
+                                    {field.showMap ? 'Hide Map' : 'Show Map'}
+                                </button>
+                                {field.showMap && (
+                                    <iframe
+                                        src={field.mapEmbedUrl}
+                                        width="600"
+                                        height="450"
+                                        title={field.name}
+                                        allowFullScreen=""
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    ></iframe>
+                                )}
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
             {/* Rest of the code... */}
-        </div >
+        </div>
     );
 };
 
