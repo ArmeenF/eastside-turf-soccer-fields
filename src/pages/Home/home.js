7680af1a2dd6dfffd5ebb389b56b5ed8416b1121
armeen <armeenfarange@yahoo.com>
2023-06-17 20:45:13 -0700

    Add iframe title to remove build error

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -45,6 +45,7 @@ const Home = () => {
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1343.4135928757325!2d-122.20061533742911!3d47.668353494764354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549013e01728085f%3A0x6c5821dad5367e3b!2sLakeview%20Elementary%20Sports%20Fields!5e0!3m2!1sen!2sus!4v1687050099318!5m2!1sen!2sus"
                                     width="600"
                                     height="450"
+                                    title="Lakeview Elementary Sports Fields"
                                     allowFullScreen=""
                                     loading="lazy"
                                     referrerPolicy="no-referrer-when-downgrade"
